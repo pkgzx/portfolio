278131ddf1ecd5f1e1d19e9e68c7a344c869fbe5
cos4h <olvadis2004@gmail.com>
2024-08-13 19:12:38 -0500

    deploy

--- src/shaders/firebg/Firebg.tsx
+++
@@ -1,5 +0,0 @@
-function Firebg({ mountRef }: { mountRef: React.RefObject<HTMLDivElement> }) {
-    return <div className='fix top-0 h-full w-full' id='firebg' ref={mountRef}></div>
-}
-
-export default Firebg
--- src/utilities/getClampedValue.ts
+++
@@ -1,3 +0,0 @@
-export const getClampedValue = (num: number, min: number, max: number) => {
-    return Math.min(Math.max(num, min), max)
-}
